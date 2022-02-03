c96fb0abeea17ef55211cb9de32d26fafe455f4f
Aaron Armstrong <aarmstrong@prometsource.com>
2022-02-03 11:27:00 -0500

    Fix script.

--- web/themes/custom/provus_bootstrap/scripts/create_subtheme.sh
+++ web/themes/custom/provus_bootstrap/scripts/create_subtheme.sh
@@ -3,30 +3,35 @@
 
 echo '
 +------------------------------------------------------------------------+
-| With this script you could quickly create provus 2.0 sub-theme     |
+| With this script you could quickly create a Provus 2.0 sub-theme       |
 | In order to use this:                                                  |
-| - provus theme (this folder) should be in the contrib folder   |
+| - provus theme (this folder) should be in the custom folder            |
 +------------------------------------------------------------------------+
 '
-echo 'The machine name of your custom theme? [e.g. myCUSTOM_PROVUS]'
+echo 'The machine name of your custom theme? [e.g. CUSTOM_PROVUS]'
 read CUSTOM_PROVUS
 
-echo 'Your theme name ? [e.g. My custom Provus site]'
+echo 'Your theme name ? [e.g. My Custom Provus Site]'
 read CUSTOM_PROVUS_NAME
 
 if [[ ! -e ../../custom ]]; then
     mkdir ../../custom
 fi
 cd ../../custom
-cp -r ../contrib/provus $CUSTOM_PROVUS
+cp -r ../custom/provus_bootstrap $CUSTOM_PROVUS
 cd $CUSTOM_PROVUS
-for file in *provus.*; do mv $file ${file//provus/$CUSTOM_PROVUS}; done
-for file in config/*/*provus.*; do mv $file ${file//provus/$CUSTOM_PROVUS}; done
+for file in *provus_bootstrap.*; do mv $file ${file//provus_bootstrap/$CUSTOM_PROVUS}; done
+for file in config/*/*provus_bootstrap.*; do mv $file ${file//provus_bootstrap/$CUSTOM_PROVUS}; done
 
 # Remove create_subtheme.sh file, we do not need it in customized subtheme.
 rm scripts/create_subtheme.sh
 
 # mv {_,}$CUSTOM_PROVUS.theme
-grep -Rl provus .|xargs sed -i -e "s/provus/$CUSTOM_PROVUS/"
-sed -i -e "s/Provus Starter Kit Subtheme/$CUSTOM_PROVUS_NAME/" $CUSTOM_PROVUS.info.yml
+grep -Rl Provus .|xargs sed -i -e "s/provus_bootstrap/$CUSTOM_PROVUS/"
+sed -i -e "s/Provus Bootstrap/$CUSTOM_PROVUS_NAME/" $CUSTOM_PROVUS.info.yml
 echo "# Check the themes/custom folder for your new sub-theme."
+
+# Remove the originals
+cd ../$CUSTOM_PROVUS
+rm $CUSTOM_PROVUS.info.yml'-e'
+rm 'README'.txt'-e'
