be72d3d6c81a3642a09312749cd47f9730b4823e
Steve Zipfel <steve@studiosixthirteen.com>
2022-02-19 13:46:47 -0500

    Update to PR / MN creation. (#34)
    * Update to PR / MN creation.
    
    * create local branch that matches new multidev branch.
    
    * Add some output for debugging.
    
    * Fix typo.
    
    * Fix yet another typo.
    
    * Add force for push to pantheon.
    
    * Add delete MD before building.
    
    * Update the PR push P_ENV value.
    
    * Update git add commands.
    
    * Add config dir.
    
    * Remove .git from menu_block module after composer install.
    
    * Lower sleep time to 60 seconds for pantheon push.
    
    * Fix typo in path for menu_block module.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -17,41 +17,52 @@ quiet_git() {
 
 # #If not the defined branch - IE: PR submissions.. create a new MD and push there... ya? 
 
-# echo "Logging into Terminus"
+echo "Logging into Terminus"
 $TERMINUS_BIN auth:login --machine-token=$SECRET_TERMINUS_TOKEN
 $TERMINUS_BIN connection:set $PANTHEON_SITE_ID.$PANTHEON_ENV git
 
 echo "Add pantheon repo"
 git remote add pantheon $PANTHEON_REPO
 
-# echo "Waking Pantheon $PANTHEON_SITE_ID Dev environment."
+echo "Waking Pantheon $PANTHEON_SITE_ID Dev environment."
 $TERMINUS_BIN env:wake -n $PANTHEON_SITE_ID.$PANTHEON_ENV
 
 echo "...Run composer install"
 composer install
+
+echo "..remove .git dir from menu_block module"
+rm -rf web/modules/contrib/menu_block/.git
 
 if [ "$CURRENT_BRANCH" != "$PANTHEON_ENV" ]; then
     # if [ `git branch --list $branch_name` ]
     # then
     # echo "Branch name $branch_name already exists."
     # fi
-  echo "...Pushing code to pantheon repo"
 
-  echo "...Building Mutlidev"
+  echo "...Building Mutlidev ci-$TRAVIS_PULL_REQUEST"
+  echo "... delete MD if it already exists"
+  $TERMINUS_BIN multidev:delete $PANTHEON_SITE_ID.ci-$TRAVIS_PULL_REQUEST --delete-branch --yes
   $TERMINUS_BIN multidev:create $PANTHEON_SITE_ID.$PANTHEON_ENV ci-$TRAVIS_PULL_REQUEST --yes
 
-  git checkout ci-$TRAVIS_PULL_REQUEST
-  git add -f vendor web
-  git commit -m "Artifacts for build ci-$TRAVIS_PULL_REQUEST"
-  git push pantheon ci-$TRAVIS_PULL_REQUEST
- 
- #determine if develop branch and merge from what PR ID
- #$TERMINUS_BIN multidev:delete $PANTHEON_SITE_ID.ci-$SOME_PR_ID --delete-branch
+  cp hosting/pantheon/* .
+  echo "...switch to new ci-$TRAVIS_PULL_REQUEST branch locally"
+  git checkout -b ci-$TRAVIS_PULL_REQUEST
+  echo "...add the new files"
+  quiet_git add -f vendor/* web/* pantheon*
+  quiet_git commit -m "Artifacts for build ci-$TRAVIS_PULL_REQUEST"
+  echo "...push to pantheon"
+  git push pantheon ci-$TRAVIS_PULL_REQUEST --force
+  P_ENV="ci-$TRAVIS_PULL_REQUEST"
+  sleep 60
 else 
   echo "...copy pantheon files to root"
   cp hosting/pantheon/* .
   git checkout $PANTHEON_ENV
-  git add -f vendor web pantheon*
-  git commit -m "TRAVIS JOB: $TRAVIS_JOB_ID - $TRAVIS_COMMIT_MESSAGE"
+  quiet_git add -f vendor web pantheon*
+  quiet_git commit -m "TRAVIS JOB: $TRAVIS_JOB_ID - $TRAVIS_COMMIT_MESSAGE"
   git push pantheon $PANTHEON_ENV --force
+  p_ENV = $PANTHEON_ENV
 fi
+
+#$TERMINUS_BIN drush -n $PANTHEON_SITE_ID.$P_ENV cim -y
+$TERMINUS_BIN drush -n $PANTHEON_SITE_ID.$P_ENV updb -y
