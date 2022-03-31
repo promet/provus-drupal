14f56ace2f608ca6060fdddc9b35f553111d8091
Steve Zipfel <steve@studiosixthirteen.com>
2022-03-31 10:47:54 -0400

    Update settings files for config location.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -11,3 +11,5 @@ if (file_exists($app_root . '/' . $site_path . '/settings.pantheon.php')) {
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+$settings['config_sync_directory'] = '../config/default';
